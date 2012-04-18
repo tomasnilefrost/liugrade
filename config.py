58cea58c49a3a09dd04d51b836ab29d91240a66c
Tomas Frostensson <tomas@frostensson.com>
2012-04-18 11:36:42 +0200

    Updated with detection of what have been changed, email will be sent

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 config_username = 'test'
 config_password = 'test'
+config_email_to = 'test@test'
+config_email_from = 'test@test'
+config_email_subject = 'Changes has been made @ Studentportalen'
+config_email_host = 'localhost'
+config_dump_filename = 'json_dump_grades'
+
--- liugrade.py
+++ liugrade.py
@@ -3,13 +3,33 @@ import operator
 import json
 from bs4 import BeautifulSoup
 from urllib import urlencode
-from config import * # pass & pwd variables
+from config import * # pass & pwd variables and so on
+import smtplib
+import string
 
 def strip_content(elem):
 	stripped = elem.renderContents().strip()
 	stripped = stripped.replace('*', '')
 	stripped = stripped.replace('\xc2\xa0', '')
 	return stripped
+
+def format_course(code, info):
+	output_string = code + ": " + info['course'] + " grade: " + info['grade'] + " points: " + info['points'] + " date: " + info['date'] + "\n"
+	if 'course_moments' in info:
+		for key in info['course_moments']:
+			output_string += "\t" + key + " grade: " + info['course_moments'][key]['grade'] + " points: " + info['course_moments'][key]['points'] + " date: " + info['course_moments'][key]['date'] + "\n"
+	return output_string
+
+old_dict = {}
+try:
+	f = open(config_dump_filename, 'r')
+	content = f.read()
+	old_dict = json.loads(content)
+	f.close()
+except:
+	# we will send mail about all the course
+#	print "Couldn't open old dictionary"
+
 
 http = httplib2.Http()
 url = 'https://www3.student.liu.se/portal/login'
@@ -34,27 +54,55 @@ for row in soup.findAll("tr"):
 	col = row.findAll("td")
 	if (len(col) == 5):
 		if col[0].findAll('b'): # a course is bold
-			data = {'course_code' : strip_content(col[0].findAll('b')[0]), 'course' : strip_content(col[1]), 'points' : strip_content(col[2]), 'grade' : strip_content(col[3]), 'date' : strip_content(col[4])}
+			data = { 'key' : strip_content(col[0].findAll('b')[0]), 'data' : {} }
+			data['data'] = {'course' : strip_content(col[1]), 'points' : strip_content(col[2]), 'grade' : strip_content(col[3]), 'date' : strip_content(col[4])}
 			course_data.append(data)
 		else: # otherwise its a course moment
 			key = strip_content(col[0]) 
-			if ('course_moments' not in course_data[-1]):
-				course_data[-1].update({'course_moments' : { } })
+			if ('course_moments' not in course_data[-1]['data']):
+				course_data[-1]['data'].update({'course_moments' : { } })
 			data = {'points' : strip_content(col[2]), 'grade' : strip_content(col[3]), 'date' : strip_content(col[4])}
-			course_data[-1]['course_moments'][key] = data
+			course_data[-1]['data']['course_moments'][key] = data
 
-print course_data
-print "\n\n\n"
+# convert to dict
+course_dict = { }
+for x in course_data:
+	course_dict.update({x['key'] : x['data']})
 
-course_data.sort(key=operator.itemgetter('course_code'))
-print course_data
+# list of new courses added
+new_course = [course for course in course_dict if course not in old_dict]
+# we will format a mail string
+mail_str = ""
+if (len(new_course) != 0):
+	mail_str = "New courses added:\n"
+	for x in new_course:
+		mail_str += format_course(x, course_dict[x])
+		# copy the new courses to the old dict to compare individually
+		old_dict.update({x : course_dict[x]})
+	mail_str += "\n"
 
+# check for changes in the existing courses (for instance added course moments)
+for x in course_dict:
+	if 'course_moments' not in course_dict[x] and 'course_moments' in old_dict[x]:
+		mail_str += "Course " + x + " has been finished...\n"
+		mail_str += format_course(x, course_dict[x])
+	elif 'course_moments' in course_dict[x] and 'course_moments' in old_dict[x]:
+		if len(course_dict[x]['course_moments']) > len(old_dict[x]['course_moments']):
+			mail_str += "Course " + x + " has been updated...\n"
+			mail_str += format_course(x, course_dict[x])
 
-js = json.dumps(course_data)
-f = open('json_dump', 'r')
-old_data = f.read()
-f.close()
-if (old_data != js):
-	print "The data differs..."
-	f = open('json_dump', 'w')
-	f.write(js)
+# we have produced output, thus we shall mail the user and update our dump file
+if (mail_str != ""):
+	f = open(config_dump_filename, 'w')
+	f.write(json.dumps(course_dict))
+	f.close()
+	body = string.join((
+			"From: %s" % config_email_from,
+			"To: %s" % config_email_to,
+			"Subject: %s" % config_email_subject ,
+			"",
+			mail_str
+			), "\r\n")
+	server = smtplib.SMTP(config_email_host)
+	server.sendmail(config_email_from, [config_email_to], body)
+	server.quit()
